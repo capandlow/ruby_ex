aebc77e0bc5bfa6bf9b0131e38dc5e05ccdefc2b
capandlow <capandlow@gmail.com>
2014-06-23 20:29:43 +0900

    ex08 directory created ex0801.rb -> ex0805.rb added

---
+++ ex08/ex0801.rb
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+title = "実践アジャイル　ソフトウェア開発法とプロジェクト管理"
+author = "山田　正樹"
+yomi = "やまだ　まさき"
+publisher = "ソフトウェアリサーチセンター"
+
+puts title
+puts author
+puts yomi
+puts publisher
---
+++ ex08/ex0802.rb
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+title = "実践アジャイル　ソフトウェア開発法とプロジェクト管理"
+author = "山田　正樹"
+yomi = "やまだ　まさき"
+publisher = "ソフトウェアリサーチセンター"
+
+puts "書籍名：　" + title
+puts "著者名：　" + author
+puts "よみがな：　" + yomi
+puts "出版社：　" + publisher
