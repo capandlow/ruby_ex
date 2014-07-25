2ffca5440bf0f09be7e5055881e0357921203be9
Hiroaki Tagawa <capandlow@gmail.com>
2014-07-25 10:58:33 +0900

    蔵書データ登録処理追加

--- bookinfo/bookinfo_web.rb
+++ bookinfo/bookinfo_web.rb
@@ -26,22 +26,44 @@ server = WEBrick::HTTPServer.new(config)
 
 server.config[:MimeTypes]["erb"] = "text/html"
 
+# 一覧表示処理
 server.mount_proc("/list") { |req, res|
    p req.query
    if /(.*)\.(delete|edit)$/ =~ req.query['operation']
 	  target_id = $1
 	  operation = $2
 	  if operation == 'delete'
-		 templete = ERB.new(File.read('delete.erb'))
+		 template = ERB.new(File.read('delete.erb'))
 	  elsif operation == 'edit'
-		 templete = ERB.new(Fire.read('edit.erb'))
+		 template = ERB.new(File.read('edit.erb'))
 	  end
-	  res.body << templete.result(binding)
+	  res.body << template.result(binding)
    else
-	  templete = ERB.new(Fire.read('noselected.erb'))
-	  res.body << templete.result(binding)
+	  template = ERB.new(File.read('noselected.erb'))
+	  res.body << template.result(binding)
    end
 
+}
+
+# 登録処理
+server.mount_proc("/entry") { |req, res|
+   p req.query
+   dbh = DBI.connect('DBI:SQLite3:bookinfo_sqlite.db')
+
+   rows = dbh.select_one("select * from bookinfos where id='#{req.query['id']}';")
+   if rows then
+	  dbh.disconnect
+
+	  template = ERB.new(File.read('noentried.erb'))
+	  res.body << template.result(binding)
+   else
+	  dbh.do("insert into bookinfos values ('#{req.query['id']}', '#{req.query['title']}', '#{req.query['author']}', '#{req.query['page']}', '#{req.query['publish_date']}');")
+
+	  dbh.disconnect
+
+	  template = ERB.new(File.read('entried.erb'))
+	  res.body << template.result(binding)
+   end
 }
 
 trap(:INT) do
